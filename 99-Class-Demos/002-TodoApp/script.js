ef4fc22a6f7fb167a6cb8870c65ea8196bddc461
Piyush Mehta <pmehta@austincc.edu>
2019-04-08 16:15:40 -0500

    Added GET and Cors

--- 99-Class-Demos/002-TodoApp/script.js
+++ 99-Class-Demos/002-TodoApp/script.js
@@ -1,11 +1,43 @@
 // THIS CODE RUNS IN THE BROWSER
 
+var todoAPIurl = "https://whispering-inlet-10068.herokuapp.com/todos";
+
+$('document').ready(function(){
+    $.ajax({
+        "url" : todoAPIurl,
+        "method": "GET"
+    })
+
+    .done(function(dataObj){    // success
+        // empty the existing todos
+        $("ul").empty();
+        // loop over the array we received from API
+        dataObj.map(function(todo){
+           let completed = todo.isComplete ? "completed" : "";
+           // let's add li's to the ul in the app
+           $("ul").append(
+             `<li class=${completed}> 
+                ${todo.description}
+                <span><i class="fa fa-times"></i></span>
+             </li>`
+           )
+        })
+    })   
+    
+    .fail(function(errorObj){   // error condition
+        // what to do if the call fails?
+        alert( "Something wrong. Please call support. Err is " + JSON.stringify(errorObj) );
+        return false;
+    })
+})
+  
+
 // UPDATE
 $('ul').on('click', 'li', function() {
   $(this).toggleClass('abcd');
 });
 
-// DELETE
+// DELETEhero
 $('ul').on('click', 'span', function(event) {
   $(this).parent().remove();
 });
--- 99-Class-Demos/003-TodoAPI/server.js
+++ 99-Class-Demos/003-TodoAPI/server.js
@@ -5,10 +5,11 @@ var morgan = require('morgan')
 app.use(morgan('dev'))
 
 var bodyParser = require('body-parser')
-
 app.use(bodyParser.json())
-
 app.use(bodyParser.urlencoded({extended: true}))
+
+var cors = require('cors')
+app.use(cors())
 
 var port = process.env.PORT || 3000
 
