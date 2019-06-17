2a04aae5d182092730a06c402c9b083be0a23ab8
Piyush <pmehta@austincc.edu>
2019-06-17 12:09:27 -0500

    added callback solution to async

--- 99-Class-Demos/async/script.js
+++ 99-Class-Demos/async/script.js
@@ -1,33 +1,37 @@
 const items = [
     {"name": "ear rings", quantity : 2},
     {"name": "necklaces", quantity : 6}
-]
-
-const toInitCase = (str) => {
-return str.split(" ")
+ ]
+ 
+ const toInitCase = (str) => {
+ return str.split(" ")
     .map(s => s.charAt(0).toUpperCase()
             + s.slice(1))
     .join(" ");
-}
+ }
+ 
+ var rootDivNode = document.getElementById("root");
+ 
+ console.log('rootDivNode is ', rootDivNode)
+ // continued on next box
+ 
 
-var rootDivNode = document.getElementById("root");
-console.log('rootDivNode is ', rootDivNode)
 
-// continued ..
-function getItems(){
-   htmlStr = '';
-   doneNode = document.createTextNode("Done");
-   // setTimeout is used as async command
-   setTimeout(function(){
-      items.forEach(function(item){
-         htmlStr += `<li>${toInitCase(item.name)}</li>`
-      })
-   rootDivNode.innerHTML = htmlStr;
-   }, 1000)
-}
+// continued
+// order of statements is solved by using cb function
+function getItems(items, cb){
+    var htmlStr = '';
+    // setTimeout is used as async command
+    items.forEach(function(item){
+        htmlStr += `<li>${toInitCase(item.name)}</li>`
+    })
 
-// async command, gets executed in the next tick
-getItems();
-
-// sync command, gets executed first
-rootDivNode.insertAdjacentHTML("afterend", "<p>Done</p>");
+    setTimeout(function(){
+      rootDivNode.innerHTML = htmlStr;
+      cb();  // callback executed after list items added
+    }, 2000)
+ }
+ 
+ getItems(items, function(){
+    rootDivNode.insertAdjacentHTML("afterend", "<p>Done</p>");
+ });
