05c513169cbe6d8ed34f07b56da390ae3c15d73d
pmehtaaustin <pmehta@austincc.edu>
2019-07-15 21:31:52 -0500

    changed port on react deploy

--- 99-Class-Demos/reactdeploy/serve.js
+++ 99-Class-Demos/reactdeploy/serve.js
@@ -8,4 +8,6 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(9000);
+app.listen(3000, function(){
+  console.log('create react app serving on port 3000')
+});
