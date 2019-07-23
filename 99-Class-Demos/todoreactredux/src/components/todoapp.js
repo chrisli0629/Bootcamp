00f6a6c911e9bb743328dbadf4d1e239220992ff
Piyush <pmehta@austincc.edu>
2019-07-22 22:54:59 -0500

    redux demo - step 4 - added addItem, deleteItem

--- 99-Class-Demos/todoreactredux/src/components/todoapp.js
+++ 99-Class-Demos/todoreactredux/src/components/todoapp.js
@@ -2,46 +2,83 @@ import React from 'react'
 import TodoList from './todolist.js'
 
 class TodoApp extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            todos: []
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      todos: [
+        // {
+        //   id: 1,
+        //   text: "Call mom"
+        // }
+      ]
+    }
+  }
+
+  addItem = (event) =>  {
+    // 0. do nothing if the field is empty
+    if(!this.inputNode.value){ return }
+
+    // 1. prevent default behavior of the form element
+    event.preventDefault()
+
+    // 2. build a new todo object with the user's input        
+    var newTodo = {
+        id: Date.now(),
+        text: this.inputNode.value
     }
 
-    addItem(){
+    // 3. update the state with the new todo
+    //    using the callback form of setState
+    this.setState(function(previousState){
+        var previousTodos = previousState.todos
+        var newTodos = previousTodos.push(newTodo)
+        return newTodos
+    })
 
-    }
+    // 4. empty out the input box
+    this.inputNode.value = "" 
+  }
 
-    deleteItem(){
+  deleteItem = (id) => {
+    // 0. get the todos from the state
+    var previousTodos = [...this.state.todos]
 
-    }
+    // 1. filter out the deleted todo object
+    var filteredTodos = previousTodos.filter(function(todo){
+        return todo.id !== id
+    })
 
-    render(){
-        return (
-            <div>
-                <div>
-                    Todo App in React
-                </div>
-                <form onSubmit={this.addItem}>
-                    <input 
-                        type="text" 
-                        placeholder="What do you need to do?" 
-                        ref={(node)=>{
-                            this.inputNode = node;
-                        }}
-                    />
-                    <button type="submit">Add</button> 
-                </form>
-                <div>
-                   <TodoList 
-                        todos={this.state.todos}
-                        deleteItem={this.deleteItem}
-                   />
-                </div>
-            </div>
-        )
-    }
+    // 2. update the state with the filtered todos
+    this.setState({
+        todos: filteredTodos
+    })
+  }
+
+  render(){
+    return (
+      <div>
+        <div>
+          Todo App in React
+        </div>
+        <form onSubmit={this.addItem}>
+          <input 
+            type="text" 
+            placeholder="What do you need to do?" 
+            ref={(node)=>{
+                this.inputNode = node;
+            }}
+          />
+          <button type="submit">Add</button> 
+        </form>
+        <div>
+           <TodoList 
+             todos={this.state.todos}
+             deleteItem={this.deleteItem}
+           />
+        </div>
+      </div>
+    )
+  }
 }
 
 export default TodoApp
--- 99-Class-Demos/todoreactredux/src/components/todolist.js
+++ 99-Class-Demos/todoreactredux/src/components/todolist.js
@@ -1,13 +1,44 @@
 import React from 'react'
 
 class TodoList extends React.Component {
-    render() { 
-        return(
-            <div>
-                Todo List
-            </div>
+  render() { 
+    var todolist = this.props.todos;
+    var todolistJSX = todolist.map(
+      (todo) => {
+        return (
+          <li 
+            key={todo.id}
+            onClick={() => this.props.deleteItem(todo.id)}
+          >
+              {todo.text}
+          </li>
         )
-    }
+      }
+    )
+    return(
+      <ul>
+        {todolistJSX}
+      </ul>
+    )
+  }
+
+// Note: commenting this out as this is not that useful
+// and it is not working
+
+//   static defaultProps = {
+//     todos: [{
+//         id: 1,
+//         text: 'Visit mom on her birthday' 
+//     },
+//     {
+//         id: 2,
+//         text: 'Buy nice bouquet of flowers before visit' 
+//     }]
+//   }
+
 }
+
+
+
 
 export default TodoList;
